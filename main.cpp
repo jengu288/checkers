e5d7da8be32cc3b3fbd8a0817908b07e86382639
jengu <jen.jennifer.gu@gmail.com>
2018-11-20 13:56:46 -0800

    Added comments about how to comment

--- main.cpp
+++ main.cpp
@@ -2,6 +2,20 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    //I am a poopy face
+
+    //this is a simple explanation comment
+
+    /*** THIS IS A TITLE FOR ORGANIZING CODE ***/
+
+    /*
+     * Param:
+     * int slope - slope of line
+     *
+     * returns :
+     */
+
+    //TODO: is not usually for other people although it can be if you are passive aggressive
+    //TODO: but should be really a reminder for what you need to do
+
     return 0;
 }
